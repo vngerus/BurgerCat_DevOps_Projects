746eb5d555f1c7852021986311444876c61eb6ba
Marcecatcas <marce.catalan.castelli@gmail.com>
2024-09-15 19:26:30 -0300

    Se agrega contenedor para las diferentes comidas a mostrar

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Card from './Card';
 
 const Menu: React.FC = () => {
     return (
         <div>
-            Menu
+           <div className="p-4 sm:ml-64">
+   <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+   <Card/>
+   </div>
+</div>
+
         </div>
     );
 };
