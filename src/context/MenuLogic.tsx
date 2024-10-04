0cc881638b6ea93497556f5558be0947fead7f39
angel <angelsmithlgs@gmail.com>
2024-10-04 17:44:37 -0300

    chore: se solucionan un par de errores en home y en la logiac del menu

--- src/context/MenuLogic.tsx
+++ src/context/MenuLogic.tsx
@@ -26,13 +26,13 @@ const MenuLogic: React.FC<MenuLogicProps> = ({ title, products }) => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-8">
-      <h2 className="text-3xl font-bold mb-6">{title}</h2>
+      <h2 className="text-3xl font-bold mb-6 text-center uppercase">{title}</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((product, index) => (
           <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden min-h-[24rem] flex flex-col justify-between">
             <img src={product.image} alt={product.name} className="rounded-t-lg w-full object-cover h-48" />
             <div className="p-5 flex-grow">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{product.name}</h5>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 uppe">{product.name}</h5>
               <p className="mb-4 font-normal text-gray-700 line-clamp-3">{product.description}</p>
 
               <div className="flex justify-center">
@@ -60,12 +60,14 @@ const MenuLogic: React.FC<MenuLogicProps> = ({ title, products }) => {
                 </div>
               </div>
 
-              <button
-                className="mt-4 bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-600"
-                onClick={() => addToCart(product.name, quantities[index])}
-              >
-                Añadir al carrito
-              </button>
+              <div className="mt-4 flex justify-center">
+                <button
+                  className="bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-600"
+                  onClick={() => addToCart(product.name, quantities[index])}
+                >
+                  Añadir al carrito
+                </button>
+              </div>
             </div>
           </div>
         ))}
