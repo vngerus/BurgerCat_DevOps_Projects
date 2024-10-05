995c426b27f66218beed4f3017e529e1af073816
angel <angelsmithlgs@gmail.com>
2024-10-05 15:39:45 -0300

    fix: se elimina data antigua de firebase y se deja encripatada ehe

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"; 
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
@@ -9,10 +9,10 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db = getDatabase(app); 
 
-export { db, firebaseConfig };
+export { app, db, firebaseConfig };
