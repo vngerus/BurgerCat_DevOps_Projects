ab7e3805b777eeb813650b79a8d0e104af6f6b6a
Marcecatcas <marce.catalan.castelli@gmail.com>
2024-10-06 17:43:29 -0300

    feat: se agrega auth para login de usuarios

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"; 
 
 const firebaseConfig = {
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app); 
+const auth = getAuth(app);
 
-export { app, db, firebaseConfig };
+export { app, db, firebaseConfig, auth };
