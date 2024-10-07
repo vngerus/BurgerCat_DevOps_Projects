b391adb7407a903fbf556b394909408c4af2b4a8
angel <angelsmithlgs@gmail.com>
2024-10-07 13:19:16 -0300

    styles: se ajustan estilos de navegador del panel admin

--- src/admin/shared/Navbar.tsx
+++ src/admin/shared/Navbar.tsx
@@ -1,44 +1,55 @@
-import React, { useState } from 'react'
-import Logo from '../../assets/imgs/heroburguer.png'
+import React, { useEffect, useState } from 'react';
+import { logoburguer } from '../../assets';
 
 type NavbarProps = {
   login: boolean;
 };
-  
-  const Navbar: React.FC<NavbarProps> = ({login}) => {
-    const [user, setUser] = useState<string>('');
+
+const Navbar: React.FC<NavbarProps> = ({ login }) => {
+  const [user, setUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchedUser = "Bonnie Green";
+    setUser(fetchedUser);
+  }, []);
 
   return (
-    login ? (<nav className="bg-white border-gray-200 dark:bg-gray-900 W-100">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <div className='flex'>
-          <img src={Logo} className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-5">BurgerCat</span>
+    login ? (
+      <nav className="bg-orange-400 border-black w-full">
+        <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+          <div className="flex items-center">
+            <img src={logoburguer} className="h-8" alt="BurgerCat Logo" />
           </div>
-        
-      <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-            <span className="sr-only">Open user menu</span>
-            <img className="w-8 h-8 rounded-full" src="" alt="user photo" />
-          </button>
-          <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-            <div className="px-4 py-3">
-              <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-              <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+
+          <div className="flex-1 flex justify-center">
+            <span className="text-2xl uppercase font-bold whitespace-nowrap">
+              BurgerCat's Admin
+            </span>
+          </div>
+
+          <div className="flex items-center space-x-3">
+            <div
+              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
+              id="user-dropdown"
+            >
+              <div className="px-4 py-3">
+                <span className="block text-sm text-gray-900">{user}</span>
+              </div>
             </div>
           </div>
-      </div>
-      </div>
-    </nav>) : (<nav className="bg-white border-gray-200 dark:bg-gray-900 W-100">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <div className='flex'>
-      <img src={Logo} className="h-8" alt="Flowbite Logo" />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-5">BurgerCat</span>
-      </div>
-  </div>
-</nav>)
+        </div>
+      </nav>
+    ) : (
+      <nav className="bg-white border-gray-200 w-full">
+        <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+          <div className="flex items-center">
+            <img src={logoburguer} className="h-8" alt="BurgerCat Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap ml-5">BurgerCat</span>
+          </div>
+        </div>
+      </nav>
+    )
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
