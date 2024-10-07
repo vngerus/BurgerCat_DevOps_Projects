7187524029a20864ec09fc6f85a19a3f222d86a9
Galo89 <edgardorc1989@gmail.com>
2024-10-07 20:19:59 -0300

    del: se borran componentes sin uso

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
-export { default as Card } from './Card';
 export { default as Cart } from './Cart';
-export { default as Footer } from './Footer';
 export { default as Hero } from './Hero';
 export { default as Loader } from './Loader';
 export { default as Menu } from './Menu';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,2 @@
 export { default as Home } from './Home';
 export { default as OrderStatus } from './OrderStatus';
-export { default as OrdersDetails } from './OrdersDetails';
